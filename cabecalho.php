bbf75937536c25b63e8283c38de604eb8be4083d
DEMILSON1972 <58113929+DEMILSON1972@users.noreply.github.com>
2020-04-15 23:49:43 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,22 @@
+<div id = "cabecalho_superior">
+	<nav id="menu-cima">                          
+			<ul>
+				<li> <a href= "#" >MINHA CONTA </a>   </li>
+				<li>    </li>
+				<li>    </li>
+				<li>    </li>
+
+			</ul>
+
+
+
+	</nav>
+</div>	
+
+<div id = "cabecalho_meio">
+
+</div>	
+
+<div id = "cabecalho_inferior">
+
+</div>	
